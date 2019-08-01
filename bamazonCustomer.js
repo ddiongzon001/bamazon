7e8a719ba6451e327ef17510b7320211e800d0c8
Dealan Diongzon <dealan.diongzon@gmail.com>
2019-08-01 19:19:35 -0400

    updated code with better functions and also changed display of output

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -29,76 +29,110 @@ function displayProducts() {
         if (err) throw err;
         console.table(res);
 
+        // stores the number of products in a variable
+        let numProducts = res.length;
+
         //promps user to buy something
-        asktoBuy();
+        asktoBuy(numProducts);
     });
 };
 
 //promps user to buy something
-function asktoBuy() {
+function asktoBuy(numProducts) {
 
     inquirer.prompt([{
-        type: "input",
-        message: "Please input the item id of the product you want to buy: ",
-        name: "product_id",
-    },
-    {
-        type: "input",
-        message: "Please input the amount of product you want to purchase: ",
-        name: "quantity",
-    }]).then(function(answer){
+            type: "input",
+            message: "Please input the item id of the product you want to buy: ",
+            name: "product_id",
+        }
+        // },
+        // {
+        //     type: "input",
+        //     message: "Please input the amount of product you want to purchase: ",
+        //     name: "quantity",
+        // }
+    ]).then(function (answer) {
 
         // save the user responses in variables
         let userProductID = answer.product_id;
-        let userProductAmount = answer.quantity;
 
-        // get the user's product information from the database
-        connection.query("SELECT product_name, price, stock_quantity FROM products WHERE ?", [{item_id: userProductID}], function(err, res){
-            if (err) throw err;
-
-            // save the product information in different variables
-            let userProductName = res[0].product_name;
-            let userProductPrice = res[0].price;
-            let userProductStock = res[0].stock_quantity;
-            // console.log(res[0].product_name);
-            
-            // Check to see if the amount the user bought does not exceed the stock_quantity
-            if (userProductAmount > userProductStock){
-                console.log(`\n**ATTENTION CUSTOMER**\nWe currently do not have enough product to fulfill your wants! Please purchase a lower amount. Sorry for any inconvience.\n`);
-                asktoBuy();
-            } 
-            // if the amount the user does buy is correct, confirm with user that is the product they want to buy
-            else{
-                console.log(`\nYou want to buy ${userProductAmount} ${userProductName} for $${userProductPrice} each.`);
-                inquirer.prompt({
-                    type: "confirm",
-                    message: "Please confirm if the above information is true:",
-                    name: "orderConfirmed",
-                }).then(function(answer){
-                    
-                    // If the user's answer is confirmed, run through the updateStock function that passes through the user's product ID & the current stock
-                    if (answer.orderConfirmed){
-                        updateStock(userProductID, userProductStock, userProductAmount);
-                    }
-                    // If the user did not confirm the item was correct, tell user and tell them to ask to buy again.
-                    else{
-                        console.log("please pick the correct item");
-                        asktoBuy();
-                    }
-                })
-            }
-            
-        });
-
-        // see if the user's quantity is greater than the stock quantity
-
-        
+        checkProductID(numProducts, userProductID);
 
     });
-    
-    // connection.end();
+
 }
 
+
+// Checks if the item_id the user inputs is correct
+function checkProductID(numProducts, id) {
+
+    // Check if the user id number is within 0 and the length
+    if (id > 0 && id <= numProducts) {
+
+        inquirer.prompt({
+            type: "input",
+            message: "Please input the amount of product you want to purchase: ",
+            name: "quantity",
+        }).then(function (answer) {
+
+            let userProductAmount = answer.quantity;
+            // get the user's product information from the database
+            connection.query("SELECT product_name, price, stock_quantity FROM products WHERE ?", [{
+                item_id: id
+            }], function (err, res) {
+                if (err) throw err;
+
+                // save the product information in different variables
+                let userProductName = res[0].product_name;
+                let userProductPrice = res[0].price;
+                let userProductStock = res[0].stock_quantity;
+                // console.log(res[0].product_name);
+
+                // run the checkStock function
+                checkStock(userProductAmount, userProductName, userProductPrice, userProductStock, id, numProducts);
+
+            })
+
+        });
+
+    } else {
+        console.log(`\n**ATTENTION CUSTOMER**\nItem number ${id} is not an option. Please choose again. Sorry for any inconvience.\n`);
+        asktoBuy(numProducts);
+    }
+}
+
+// checks to see if the amount user brought does not exceed the stock_quantity of the product
+function checkStock(userProductAmount, userProductName, userProductPrice, userProductStock, id, numProducts) {
+
+    // if the user amount is greater than stock, prompt user of error
+    if (userProductAmount > userProductStock) {
+        console.log(`\n**ATTENTION CUSTOMER**\nWe currently do not have enough product to fulfill your wants! Please purchase a lower amount. Sorry for any inconvience.\n`);
+        asktoBuy();
+    }
+    // if the amount the user does buy is correct, confirm with user that is the product they want to buy
+    else {
+        console.log(`\nYou want to buy ${userProductAmount} ${userProductName} for $${userProductPrice} each.`);
+        inquirer.prompt({
+            type: "confirm",
+            message: "Please confirm if the above information is true:",
+            name: "orderConfirmed",
+        }).then(function (answer) {
+
+            // If the user's answer is confirmed, run through the updateStock function that passes through the user's product ID & the current stock
+            if (answer.orderConfirmed) {
+                updateStock(id, userProductStock, userProductAmount);
+            }
+            // If the user did not confirm the item was correct, tell user and tell them to ask to buy again.
+            else {
+                // console.log(`\n`);
+                asktoBuy(numProducts);
+            }
+        })
+    }
+
+}
+
+// Updates the stock in mysql
 function updateStock(id, stock, purchased) {
 
     //subtracts the purchase amount
